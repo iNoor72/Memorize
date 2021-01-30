3cef8db139fc7394ea08edd39d5ee95351e02c6e
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-01-30 21:49:11 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Noor Walid on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
